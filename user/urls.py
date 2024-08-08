6b64d5539e8c684f28c47850a2c8a6f110972853
Nykyta Nykolaytsev <kupcenkonikita73@gmail.com>
2024-08-08 12:34:34 +0300

    add user urls

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name='create_user'),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 app_name = 'user'
